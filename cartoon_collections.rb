6686367a9bfe1742041e3f80246f0399d26a2954
Mar Mustafa <amustaf@email.arizona.edu>
2020-07-17 18:36:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
  
 planeteer_calls.find |call| do
-  planeteer_calls.include? (valid_calls)
+  valid_calls.include? (valid_calls)
 end
 
 
